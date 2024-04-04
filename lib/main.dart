45d861af83193e38adb12660aa8b7ec50ec94145
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2024-04-04 22:28:04 +0100

    setup project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'l10n/l10n.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+
+  // FlutterNativeSplash.remove();
+
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
