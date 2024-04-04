c6404df794bdf506fe3c13b35e486f2e191b6b86
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2024-04-04 21:55:00 +0100

    setup project

---
+++ lib/utils/utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
+
+double getHeight(double value, BuildContext context) {
+  double size = 0;
+  size = value.dp;
+  return size;
+}
+
+double getWidth(double value, BuildContext context) {
+  double size = 0;
+  size = value.dp;
+  return size;
+}
